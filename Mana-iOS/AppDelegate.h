40d76f0eeb0c8a237a74ab9ed9a8406587287ba1
Oleksandr Burla <burla.dev@gmail.com>
2016-04-08 15:33:43 +0300

    Initial Commit

---
+++ Mana-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mana-iOS
+//
+//  Created by Oleksandr Burla on 4/8/16.
+//  Copyright © 2016 DB2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mana-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mana-iOS
+//
+//  Created by Oleksandr Burla on 4/8/16.
+//  Copyright © 2016 DB2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
